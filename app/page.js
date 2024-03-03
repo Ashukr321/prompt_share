c5dd55d56078010219dc31fd6b5b146ce61b6620
Ashukr321 <coderashukr321@gmail.com>
2024-03-03 13:52:44 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,19 @@
+const page = () => {
+  return (
+    <div>
+      {/* 
+      
+        next js provide many ways to fetching data 
+
+        1. client side data fetching 
+        2. isr incremental   static regeneration
+        3 ssr server side rendring 
+        4 ssg statis site generation 
+        
+       */}
+       <h1>data fetching in the next js </h1>
+    </div>
+  )
+}
+
+export default page
